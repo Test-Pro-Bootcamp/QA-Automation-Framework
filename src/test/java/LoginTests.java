78f413a0f616e2b319c0a8d8bfb0bc228defe75f
Chaanyah Laborde <laborde.chaanyah@gmail.com>
2022-10-16 10:45:48 -0400

    Update LoginTests.java

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -3,21 +3,27 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+
 public class LoginTests {
 
     @Test
-    public static void LoginEmptyEmailPasswordTest () {
+    public static void LoginEmptyEmailPasswordTest () throws InterruptedException {
 
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\jimmy\\OneDrive\\Documents\\GitHub\\QA-Automation-Framework\\src\\test\\resources\\chromedriver106.exe");
 
+        // This is for Windows users
         if (System.getProperty("os.name").toLowerCase().contains("win")) {
             System.setProperty("webdriver.chrome.driver","chromedriver.exe");
         }
 
         WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
         String url = "https://bbb.testpro.io/";
         driver.get(url);
-        Assert.assertEquals(driver.getCurrentUrl(), url);
-//        driver.quit();
+
+        Thread.sleep(5000);
+        driver.quit();
     }
 }
