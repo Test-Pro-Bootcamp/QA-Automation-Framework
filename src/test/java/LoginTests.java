c2c9b5aa9c090d85b6001249b6f7492a318d54ca
Chaanyah Laborde <laborde.chaanyah@gmail.com>
2022-10-16 11:03:40 -0400

    Update LoginTests.java

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -9,8 +11,6 @@ public class LoginTests {
 
     @Test
     public static void LoginEmptyEmailPasswordTest () throws InterruptedException {
-
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\jimmy\\OneDrive\\Documents\\GitHub\\QA-Automation-Framework\\src\\test\\resources\\chromedriver106.exe");
 
         // This is for Windows users
         if (System.getProperty("os.name").toLowerCase().contains("win")) {
@@ -26,4 +26,33 @@ public class LoginTests {
         Thread.sleep(5000);
         driver.quit();
     }
+
+    @Test
+    public static void ValidateRegistrationPage() throws InterruptedException {
+
+        if (System.getProperty("os.name").toLowerCase().contains("win")) {
+            System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+        }
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        String url = "https://bbb.testpro.io/";
+        driver.get(url);
+
+        WebElement registrationLink = driver.findElement(By.id("hel"));
+        WebElement email = driver.findElement(By.cssSelector("[type = 'email']"));
+        WebElement password = driver.findElement(By.cssSelector("[type = 'password']"));
+
+//        email.click();
+//        email.sendKeys("laborde.chaanyah@gmail.com");
+//        password.click();
+//        password.sendKeys("te$t$tudent");
+        registrationLink.click();
+
+        Thread.sleep(5000);
+        driver.quit();
+
+    }
+
 }
