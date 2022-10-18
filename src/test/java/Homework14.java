22ffa2870ce343caf09f3e3d9037b805b9450599
Salma Ayad <salma.ayad.87@gmail.com>
2022-10-17 21:19:29 -0400

    Homework14-Salma

--- src/test/java/Homework14.java
+++ src/test/java/Homework14.java
@@ -1,3 +1,4 @@
+//import dev.failsafe.internal.util.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -20,43 +21,49 @@ public class Homework14 {
 //    Copy and paste the link of the pull request (or your branch) to the field below, so we can check your homework
 
     @Test
-    public static void Homework14Solution () throws InterruptedException {
-
-        String url = "https://bbb.testpro.io/";
-        String registrationURL = "https://bbb.testpro.io/registration.php";
-
+    public static void Homework14Solution() throws InterruptedException {
 
         WebDriver driver = new ChromeDriver();
 
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.get(url);
-        driver.manage().window().maximize();
+        try {
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+            String url = "https://bbb.testpro.io/";
+            String registrationURL = "https://bbb.testpro.io/registration.php";
+            driver.get(url);
+            driver.manage().window().maximize();
 
-
-        WebElement registrationLinkElement = driver.findElement(By.id("hel"));
+            By registrationLinkSelector = By.id("hel");
+            WebElement registrationLinkElement = driver.findElement(registrationLinkSelector);
 //        WebElement textToGetUsingCssSelector = driver.findElement(By.cssSelector("font[color='#f9f9ff']"));
 
-        Thread.sleep(2000);
-        registrationLinkElement.click();
-        Thread.sleep(5000);
+            Thread.sleep(2000);
+            registrationLinkElement.click();
+
+            Thread.sleep(5000);
 
 //        first approach - assert if the current url is equal to your stored (String)  registration page url
-        Assert.assertEquals(driver.getCurrentUrl(), registrationURL);
+            Assert.assertEquals(driver.getCurrentUrl(), registrationURL);
 
 //        another approach - Print current url
-        String retrievedUrl = driver.getCurrentUrl();
-        System.out.println("Our current URL is " +retrievedUrl);
+            String retrievedUrl = driver.getCurrentUrl();
+            System.out.println("Our current URL is " + retrievedUrl);
 
 //        another approach - check if the "Register new account" text/line which can be found in the registration page is displayed in the web page
-        WebElement registerNewAccountHeader = driver.findElement(By.xpath("//h2[contains(text(),'Register new account')]"));
-        boolean isHeaderPresent = registerNewAccountHeader.isDisplayed();
-        System.out.println(isHeaderPresent); // returns true if element is displayed
-
-        driver.quit();
+            WebElement registerNewAccountHeader = driver.findElement(By.xpath("//h2[contains(text(),'Register new account')]"));
+            boolean isHeaderPresent = registerNewAccountHeader.isDisplayed();
+            if (isHeaderPresent) {
+                System.out.println("We are on the right page");
+            }
+            // returns true if element is displayed
+        } finally {
+            driver.quit();
+        }
     }
+}
 
 
-    }
+
+
 
 
 
