3ecba75c065c94bfe4f15f16e91864f487cb7150
mmeza929 <mmeza2995@gmail.com>
2022-10-18 20:55:16 -0500

    HW14

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -82,5 +82,44 @@ public class LoginTests {
     }
 
 
+    @Test
+    public static void Homework14Solution () throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.manage().window().maximize();
+
+
+        String url = "https://bbb.testpro.io/";
+        String registrationURL = "https://bbb.testpro.io/registration.php";
+        driver.get(url);
+
+
+        WebElement registrationLinkElement = driver.findElement(By.id("hel"));
+//   WebElement textToGetUsingCssSelector = driver.findElement(By.cssSelector("font[color='#f9f9ff']"));
+
+        Thread.sleep(2000);
+        registrationLinkElement.click();
+        Thread.sleep(5000);
+
+//        first approach - assert if the current url is equal to your stored (String)  registration page url
+        //  Assert.assertEquals(driver.getCurrentUrl(), registrationURL);
+
+//        another approach - Print current url
+        String retrievedUrl = driver.getCurrentUrl();
+        System.out.println("Our current URL is " +retrievedUrl);
+
+//        another approach - check if the "Register new account" text/line which can be found in the registration page is displayed in the web page
+        //  WebElement registerNewAccountHeader = driver.findElement(By.xpath("//h2[contains(text(),'Register new account')]"));
+        //     boolean isHeaderPresent = registerNewAccountHeader.isDisplayed();
+        //     System.out.println(isHeaderPresent); // returns true if element is displayed
+
+        driver.quit();
+    }
+
+
+
 
 }
