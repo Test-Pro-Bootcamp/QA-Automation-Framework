95103270ac0bbd7d8aac277034b1e10ce416ca79
mmeza929 <mmeza2995@gmail.com>
2022-10-18 19:50:56 -0500

    HW14

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -8,14 +10,77 @@ import java.time.Duration;
 public class LoginTests {
 
     @Test
-    public static void LoginEmptyEmailPasswordTest () {
+    public static void LoginEmptyEmailPasswordTest() throws InterruptedException {
 
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         String url = "https://bbb.testpro.io/";
         driver.get(url);
-        Assert.assertEquals(driver.getCurrentUrl(), url);
+        Thread.sleep(5000);
         driver.quit();
     }
+
+    @Test
+    public static void LoginValidEmailValidPasswordTest() throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        String url = "https://bbb.testpro.io/";
+        driver.get(url);
+
+        WebElement emailField = driver.findElement(By.cssSelector("[type = 'email']"));
+        emailField.click();
+        emailField.sendKeys("demo@class.com");
+
+        WebElement passwordField = driver.findElement(By.cssSelector("[type = 'password']"));
+        passwordField.click();
+        passwordField.sendKeys("te$t$tudent");
+
+       WebElement submit = driver.findElement(By.cssSelector("[type = 'submit']"));
+       submit.click();
+
+       WebElement avatar = driver.findElement(By.cssSelector("[alt = 'Avatar of student']"));
+        Assert.assertTrue(avatar.isDisplayed());
+
+        Thread.sleep(5000);
+        driver.quit();
+    }
+
+    @Test
+    public static void LoginInvalidEmailValidPasswordTest() throws InterruptedException {
+
+
+        WebDriver driver = new ChromeDriver();
+        try {
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+            String url = "https://bbb.testpro.io/";
+            driver.get(url);
+
+            WebElement emailField = driver.findElement(By.cssSelector("[type = 'email']"));
+            emailField.click();
+            emailField.sendKeys("dem@class.com");
+
+            WebElement passwordField = driver.findElement(By.cssSelector("[type = 'password']"));
+            passwordField.click();
+            passwordField.sendKeys("te$t$tudent");
+
+            WebElement submit = driver.findElement(By.cssSelector("[type = 'submit']"));
+            submit.click();
+
+            WebElement avatar = driver.findElement(By.cssSelector("[alt = 'Avatar of student']"));
+            Assert.assertTrue(avatar.isDisplayed());
+
+            Thread.sleep(5000);
+        }
+        finally{
+            driver.quit();
+        }
+
+    }
+
+
+
 }
