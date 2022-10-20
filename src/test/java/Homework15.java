871d55f6b06c9331f79e8ec2a8ea4a490b20d2de
Salma Ayad <salma.ayad.87@gmail.com>
2022-10-20 10:04:23 -0400

    Update Homework15.java

--- src/test/java/Homework15.java
+++ src/test/java/Homework15.java
@@ -10,36 +10,36 @@ import java.time.Duration;
 
 public class Homework15 {
     @Test
-    public static void Homework15solution() throws InterruptedException {
+    public static void checkSong() throws InterruptedException {
         WebDriver driver = new ChromeDriver();
-        try {
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-            String url = "https://bbb.testpro.io/";
-            driver.get(url);
+        String url = "https://bbb.testpro.io/";
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get(url);
 
-            WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
-            emailField.click();
-            emailField.sendKeys("salma.ayad.87@gmail.com");
+        WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
+        emailField.click();
+        emailField.sendKeys("salma.ayad.87@gmail.com");
 
-            WebElement passwordField = driver.findElement(By.cssSelector("[type='password']"));
-            passwordField.click();
-            passwordField.sendKeys("te$t$tudent");
+        WebElement passwordField = driver.findElement(By.cssSelector("[type='password']"));
+        passwordField.click();
+        passwordField.sendKeys("te$t$tudent");
 
-            WebElement submitButton = driver.findElement(By.cssSelector("[type='submit']"));
-            submitButton.click();
+        WebElement submitButton = driver.findElement(By.cssSelector("[type='submit']"));
+        submitButton.click();
 
 
-            WebElement searchField = driver.findElement(By.cssSelector("[name='q']"));
-            searchField.click();
-            searchField.sendKeys("Pluto");
-            Thread.sleep(5000);
-           //Validating the song matches results
-            WebElement searchWrapper = driver.findElement(By.id("searchExcerptsWrapper"));
-            searchWrapper.getText();
-            System.out.println(searchWrapper.getText().contains("Pluto"));
-        } finally {
+        WebElement searchField = driver.findElement(By.cssSelector("[name='q']"));
 
-            driver.quit();
-        }
+
+        searchField.click();
+        searchField.sendKeys("Pluto");
+        Thread.sleep(5000);
+        //Validating the song matches results
+        WebElement searchWrapper = driver.findElement(By.id("searchExcerptsWrapper"));
+        searchWrapper.getText();
+        System.out.println(searchWrapper.getText().contains("Pluto"));
+
+        driver.quit();
+
     }
 }
