01c99cd021f4f4077985f09f058fab0607521edd
jlmcswain <108906957+jlmcswain@users.noreply.github.com>
2022-10-20 12:24:27 -0700

    class selenium

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -8,16 +10,111 @@ import java.time.Duration;
 public class LoginTests {
 
     @Test
-    public static void LoginEmptyEmailPasswordTest () {
+    public static void LoginValidEmailValidPasswordTest() {
 
         WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        try {
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        String url = "https://bbb.testpro.io/";
-        driver.get(url);
-        Assert.assertEquals(driver.getCurrentUrl(), url);
-        driver.quit();
+            String url = "https://bbb.testpro.io/";
+            driver.get(url);
 
+            WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
+            emailField.click();
+            emailField.sendKeys("demo@class.com");
 
+            WebElement passwordField = driver.findElement(By.cssSelector("[type='password']"));
+            passwordField.click();
+            passwordField.sendKeys("te$t$tudent");
+
+            WebElement submitButton = driver.findElement(By.cssSelector("[type='submit']"));
+            submitButton.click();
+
+            WebElement avatarIcon = driver.findElement(By.cssSelector("[alt = 'Avatar of student']"));
+            Assert.assertTrue(avatarIcon.isDisplayed());
+        }
+            finally {
+            driver.quit();
+        }
     }
+
+    @Test
+    public static void LoginInValidEmailPasswordTest() {
+
+        WebDriver driver = new ChromeDriver();
+        try {
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+            String url = "https://bbb.testpro.io/";
+            driver.get(url);
+
+            WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
+            emailField.click();
+            emailField.sendKeys("dem@class.com");
+
+            WebElement passwordField = driver.findElement(By.cssSelector("[type='password']"));
+            passwordField.click();
+            passwordField.sendKeys("te$t$tudent");
+
+            WebElement submitButton = driver.findElement(By.cssSelector("[type='submit']"));
+            submitButton.click();
+
+            WebElement avatarIcon = driver.findElement(By.cssSelector("[alt = 'Avatar of student']"));
+            Assert.assertTrue(avatarIcon.isDisplayed());
+        }
+        finally {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public static void LoginValidEmailEmptyPasswordTest() {
+
+        WebDriver driver = new ChromeDriver();
+        try {
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+            String url = "https://bbb.testpro.io/";
+            driver.get(url);
+
+            WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
+            emailField.click();
+            emailField.sendKeys("demo@class.com");
+
+            WebElement passwordField = driver.findElement(By.cssSelector("[type='password']"));
+            passwordField.click();
+            passwordField.sendKeys("");
+
+            WebElement submitButton = driver.findElement(By.cssSelector("[type='submit']"));
+            submitButton.click();
+
+            WebElement avatarIcon = driver.findElement(By.cssSelector("[alt = 'Avatar of student']"));
+            Assert.assertTrue(avatarIcon.isDisplayed());
+        }
+        finally {
+            driver.quit();
+        }
+    }
+    @Test
+    public static void LoginValidEmailEmptyPasswordTest() {
+
+        WebDriver driver = new ChromeDriver();
+        try {
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+            String url = "https://bbb.testpro.io/";
+            driver.get(url);
+
+            WebElement searchBar = driver.findElement(By.cssSelector("[type='search']"));
+            searchBar.click();
+            searchBar.sendKeys("Dark Days");
+            
+
+            WebElement avatarIcon = driver.findElement(By.cssSelector("[alt = 'Avatar of student']"));
+            Assert.assertTrue(avatarIcon.isDisplayed());
+        } finally {
+            driver.quit();
+        }
+    }
+
 }
