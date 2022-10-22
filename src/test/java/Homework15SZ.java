96f0d60ac27278836b1c2214c9d79207f5d5549a
StephenZacheus <114630406+StephenZacheus@users.noreply.github.com>
2022-10-22 00:47:48 -0700

    correction
    attempt HW15

---
+++ src/test/java/Homework15SZ.java
@@ -0,0 +1,57 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+
+//        Create a new file Homework15.java file with @Test annotation in IntelliJ IDEA
+//        Create a test case search() using @Test annotation
+//        Navigate to "https://bbb.testpro.io/"
+//        Log in with your credentials
+//        Search for the song "Pluto" (use CSS locators to locate the elements)
+//        Validate that there is a song matched in a search results
+//        Create a new branch and commit your changes
+//        Push your code to a remote repository
+//        Create a pull request
+//        Copy and paste the link of the pull request (or your branch) to the field below, so we can check your homework
+public class Homework15SZ {
+    @Test
+    public static void Searchsong () throws InterruptedException {
+
+        // open Chrome browser
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); //wait for 10 second to load before railing the test
+        // to open a URL
+        String url = "https://bbb.testpro.io/";
+        driver.get(url);
+        // Locate web element for email field by css locator
+        WebElement emailField = driver.findElement(By.cssSelector("[type ='email"));
+        emailField.click();// click to element
+        emailField.sendKeys("demo@class.com");//typing demo@class.com in email field
+
+        WebElement passwordField = driver.findElement(By.cssSelector("[type ='password"));
+        passwordField.click();// click to element
+        passwordField.sendKeys("te$t$tudent");
+
+
+
+        WebElement submitButton = driver.findElement(By.cssSelector("[type ='submit"));
+        submitButton.click();// click to element
+
+//        WebElement avatarIcon = driver.findElement(By.cssSelector("[alt='Avatar of student"));
+//        Assert.assertTrue(avatarIcon.isDisplayed());
+
+        WebElement search = driver.findElement(By.cssSelector("[name='q']"));
+        search.click();
+        search.sendKeys("pluto");
+
+
+        Thread.sleep(2000);
+        driver.quit(); // to close the driver
+
+    }
+}
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,5 +1,7 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -8,14 +10,86 @@ import java.time.Duration;
 public class LoginTests {
 
     @Test
-    public static void LoginEmptyEmailPasswordTest () {
+    public static void LoginEmptyEmailPasswordTest() {
 
         WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//
+        try {
 
-        String url = "https://bbb.testpro.io/";
-        driver.get(url);
-        Assert.assertEquals(driver.getCurrentUrl(), url);
-        driver.quit();
+
+//            String url = "https://bbb.testpro.io/";
+//            driver.get(url);
+//            Assert.assertEquals(driver.getCurrentUrl(), url);
+//            driver.quit();
+
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); //wait for 10 second to load before railing the test
+            // to open a URL
+            String url = "https://bbb.testpro.io/";
+            driver.get(url);
+            // Locate web element for email field by css locator
+            WebElement emailField = driver.findElement(By.cssSelector("[type ='email"));
+            emailField.click();// click to element
+            emailField.sendKeys("demo@class.com");//typing demo@class.com in email field
+
+            WebElement passwordField = driver.findElement(By.cssSelector("[type ='password"));
+            passwordField.click();// click to element
+            passwordField.sendKeys("te$t$tudent");
+
+            WebElement submitButton = driver.findElement(By.cssSelector("[type ='submit"));
+            submitButton.click();// click to element
+
+            WebElement avatarIcon = driver.findElement(By.cssSelector("[alt='Avatar of student"));
+            Assert.assertTrue(avatarIcon.isDisplayed());
+
+//            WebElement queueLink = driver.findElement(By.cssSelector("[href=\"#!/queue\"]"));
+//            queueLink.click();
+//
+//            WebElement homeLink = driver.findElement(By.cssSelector("[href=\"#!/home\"]"));
+//            homeLink.click();
+
+//            WebElement albums = driver.findElement(By.cssSelector("[href=\"#!/albums\"] "));
+//            albums.click();
+//
+//            Assert.assertEquals(driver.getCurrentUrl(), url);
+        } finally {
+
+
+            //Thread.sleep(5000); // to make program sleep for  5 seconds
+            driver.quit(); // to close the driver
+        }
     }
-}
+
+        @Test
+        public static void LoginInvalidEmailPasswordTest() {
+
+            // open Chrome browser
+            WebDriver driver = new ChromeDriver();
+            try {
+                driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); //wait for 10 second to load before railing the test
+                // to open a URL
+                String url = "https://bbb.testpro.io/";
+                driver.get(url);
+                // Locate web element for email field by css locator
+                WebElement emailField = driver.findElement(By.cssSelector("[type ='email"));
+                emailField.click();// click to element
+                emailField.sendKeys("dem@class.com");//typing demo@class.com in email field
+
+                WebElement passwordField = driver.findElement(By.cssSelector("[type ='password"));
+                passwordField.click();// click to element
+                passwordField.sendKeys("te$t$tudent");
+
+                WebElement submitButton = driver.findElement(By.cssSelector("[type ='submit"));
+                submitButton.click();// click to element
+
+                WebElement avatarIcon = driver.findElement(By.cssSelector("[alt='Avatar of student"));
+                Assert.assertTrue(avatarIcon.isDisplayed());
+
+
+            } finally {
+
+                driver.quit(); // to close the driver
+            }
+
+        }
+    }
