d0b16e6c62d9c2669c0249a7ab33d2560e9d77c7
NozimaB <nozishka86@gmail.com>
2022-10-24 00:35:39 -0600

    HW17_Nozima
    Hoping to learn how to double click on a button/element!

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,6 +1,19 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
+import java.time.Duration;
+
 public class BaseTest {
+
+    static WebDriver driver;
+    static String newPlaylistName = "Nozima's Songs";
+
 
     @BeforeSuite
     public static void chromeConfigs() {
@@ -8,6 +21,51 @@ public class BaseTest {
         if (System.getProperty("os.name").toLowerCase().contains("win")) {
             System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         }
+    }
+
+    public static void clickSubmitBtn() {
+            WebElement loginButton = driver.findElement(By.xpath("//button[@type='submit']"));
+            loginButton.click();
+    }
+
+    public static void enterPassword(String password) {
+            WebElement passwordField = driver.findElement(By.xpath("//*[@type='password']"));
+            passwordField.click();
+            passwordField.sendKeys(password);
+    }
+
+    public static void enterEmail(String email) {
+        WebElement emailField = driver.findElement(By.xpath("//*[@type='email']"));
+            emailField.sendKeys(email);
+    }
+
+    public static void clickNewPlaylistBtn() {
+        WebElement newPButton = driver.findElement(By.cssSelector("[class='fa fa-plus-circle create']"));
+        newPButton.click();
+    }
+
+    public static void clickDropDown() {
+        WebElement dropDownOption = driver.findElement(By.xpath("//li[contains(text(), 'New Playlist')]"));
+        dropDownOption.click();
+    }
+
+    public static void enterPlayListName() {
+        WebElement playNameField = driver.findElement(By.xpath("//input[@name='name']"));
+        playNameField.sendKeys(newPlaylistName);
+        playNameField.sendKeys(Keys.ENTER);
+    }
+
+    @BeforeMethod
+    public void launchBrowser(){
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        String url = "https://bbb.testpro.io/";
+        driver.get(url);
+    }
+
+    @AfterMethod
+    public void closeBrowser(){
+        driver.quit();
     }
 
 }
---
+++ src/test/java/HW17_Nozima.java
@@ -0,0 +1,52 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class HW17_Nozima extends BaseTest {
+
+    @Test
+    public static void addSongToPlaylist() throws InterruptedException{
+
+        enterEmail("nozishka86@gmail.com");
+        enterPassword("te$t$tudent");
+        clickSubmitBtn();
+        Thread.sleep(2000);
+
+        clickNewPlaylistBtn();
+        clickDropDown();
+        enterPlayListName();
+
+        WebElement allSongs = driver.findElement(By.xpath("//a[text()='All Songs']"));
+        allSongs.click();
+        allSongs.click();
+        Thread.sleep(5000);
+
+        WebElement song = driver.findElement(By.xpath("//tr[@class='song-item']//td[contains(text(),'a Beat')]"));
+        song.click();
+
+        WebElement addToBtn = driver.findElement(By.xpath("//button[@title='Add selected songs to…']"));
+        addToBtn.click();
+        Thread.sleep(3000);
+
+        WebElement myPlaylist = driver.findElement(By.xpath("//li[@class='playlist']"));
+        myPlaylist.click();
+        Thread.sleep(3000);
+
+        WebElement nozimaPlaylist = driver.findElement(By.xpath("//a[contains(text(),'Nozima')]"));
+        nozimaPlaylist.click();
+        Thread.sleep(3000);
+
+        WebElement addedSong = driver.findElement(By.xpath("//tr[@class='song-item']//td[contains(text(),'a Beat')]"));
+        Assert.assertTrue(addedSong.isDisplayed());
+        Thread.sleep(3000);
+    }
+
+}
+
+//Create a test case addSongToPlaylist() using @Test annotations
+//and the helper/reusable methods we created
+//Navigate to "https://bbb.testpro.io/"
+//Log in with your credentials
+//Add a song to a playlist
+//Validate that there is a song added
