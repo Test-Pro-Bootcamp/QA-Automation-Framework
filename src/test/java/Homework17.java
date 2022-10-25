5dc9c2e153578a30066fc6ac2d0893b5876ed316
Salma Ayad <salma.ayad.87@gmail.com>
2022-10-25 12:57:10 -0400

    Create Homework17.java

---
+++ src/test/java/Homework17.java
@@ -0,0 +1,36 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Homework17 extends BaseTest{
+    @Test
+   public void addSongToPlaylist() throws InterruptedException{
+        provideEmail("salma.ayad.87@gmail.com");
+        providePassword();
+        clickSubmitBtn();
+        clickAllSongs();
+
+        WebElement chooseSong = driver.findElement(By.xpath("//td[text()='Mid-Air Machine - If It_s All I Do']"));
+        chooseSong.click();
+        WebElement addToBtn = driver. findElement(By.cssSelector("button.btn-add-to"));
+        addToBtn.click();
+        Thread.sleep(3000);
+
+        WebElement playlistToAddSong = driver.findElement(By.xpath("//li[@class='favorites']"));
+        playlistToAddSong.click();
+        Thread.sleep(2000);
+       WebElement PlayList = driver.findElement(By.cssSelector("li.playlist.favorites"));
+       PlayList.click();
+
+       WebElement confirmSongIsAdded = driver.findElement(By.cssSelector("#favoritesWrapper > div"));
+       Assert.assertTrue(confirmSongIsAdded.getText().contains("Mid-Air Machine"));
+    }
+
+
+    private void clickAllSongs() {
+        WebElement clickAllSongsBtn = driver.findElement(By.xpath("//a[@class='songs']"));
+        clickAllSongsBtn.click();
+    }
+}
