a6607eed65f43cb57a3c12672ea015a5d7b518c6
SGThel1sing <iq14111991@gmail.com>
2022-10-31 17:10:19 -0400

    gitDemoSample

---
+++ src/test/java/GitHubDemo.java
@@ -0,0 +1,9 @@
+public class GitHubDemo {
+
+    public static void main(String[] args) {
+        System.out.println("DEMO GITHUB");
+//        WE ARE GOOD LETS TRY TO COMMIT/PUSH IT TO GITHUB
+    }
+
+
+}
