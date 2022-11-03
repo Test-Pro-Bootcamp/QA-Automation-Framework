c214e422b9a0af4da78569f04841d3ce79ba9b18
Yana <Dankoyanka@gmail.com>
2022-11-03 12:10:20 -0500

    Homework21 Yana D.

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,14 +1,12 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
-import org.testng.annotations.BeforeSuite;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.DataProvider;
+import org.testng.annotations.*;
 import java.time.Duration;
 
 
@@ -17,15 +15,7 @@ public class BaseTest {
     static WebDriver driver;
     static String url;
     static WebDriverWait wait;
-
-    public static void browserConfigs() {
-
-        driver.get(url);
-        url = "https://bbb.testpro.io/";
-        driver.manage().window().maximize();
-        provideEmail("dankoyankaa@gmail.com");
-        providePassword("te$t$tudent");
-    }
+    static Actions action;
 
     @BeforeSuite
     public static void chromeConfigs() {
@@ -35,11 +25,14 @@ public class BaseTest {
     }
 
     @BeforeMethod
-    public void getBrowser() {
+    @Parameters({"baseURL"})
+    public void getBrowser(String baseURL) {
         driver = new ChromeDriver();
+        action = new Actions(driver);
+        wait = new WebDriverWait(driver,Duration.ofSeconds(10));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        String url = "https://bbb.testpro.io/";
-        driver.get(url);
+        driver.manage().window().maximize();
+        driver.get(baseURL);
     }
 
     @AfterMethod
@@ -50,21 +43,18 @@ public class BaseTest {
     @DataProvider(name = "loginProvider")
     public static Object[][] credentials() {
         return new Object[][]{
-                {"dankoyanka@gmail.com", "te$t$tudent" },
+                {"dankoyanka@gmail.com", "te$t$tudent"},
         };
-
     }
 
     public static void provideEmail(String email) {
-        WebElement emailField = BaseTest.driver.findElement(By.cssSelector("[type = 'email']"));
-        emailField.click();
-        emailField.sendKeys(email);
+       wait.until(ExpectedConditions.visibilityOfElementLocated
+               (By.xpath("//input[@type='email']"))).sendKeys(email);;
     }
 
     public static void providePassword(String password) {
-        WebElement passwordField = BaseTest.driver.findElement(By.cssSelector("[type = 'password']"));
-        passwordField.click();
-        passwordField.sendKeys(password);
+        wait.until(ExpectedConditions.visibilityOfElementLocated
+                (By.xpath("//input[@type='password']"))).sendKeys(password);;
     }
 
     public static void clickSubmitBtn() {
@@ -72,56 +62,32 @@ public class BaseTest {
         loginButton.click();
     }
 
-    public static void deletePlaylist()  {
-
-        WebDriverWait locatePlaylist = new WebDriverWait(driver, Duration.ofSeconds(10));
-        locatePlaylist.until(ExpectedConditions.elementToBeClickable
-                (By.xpath("//li[@class='playlist playlist']"))).click();
 
 
-        WebDriverWait clickDelete = new WebDriverWait(driver, Duration.ofSeconds(10));
-        clickDelete.until(ExpectedConditions.elementToBeClickable
-                (By.xpath("//button[@class='del btn-delete-playlist']"))).click();
-//
-//        WebDriverWait okDeleteBtn = new WebDriverWait(driver, Duration.ofSeconds(10));
-////        okDeleteBtn.until(ExpectedConditions.elementToBeClickable
-//                (By.xpath("//button[@class='ok']"))).click();
-    }
-
-
-    public WebElement getConfirmationPopUpText() {
+    public void doubleClickChoosePlaylist() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.success.show")));
-        return driver.findElement(By.cssSelector("div.success.show"));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".playlist:nth-child(3)")));
+        WebElement webElement = driver.findElement(By.cssSelector(".playlist:nth-child(3)"));
+        action.doubleClick(webElement).perform();
     }
 
-//    public WebElement confirmDelete() {
-//        WebElement confirmDelete = driver.findElement
-//                (By.xpath(" //div[@class='success show']"));
-//        Assert.assertTrue(confirmDelete.isDisplayed());
-//        return confirmDelete;
+    public void choosePlaylist() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".playlist:nth-child(3)")));
     }
 
+    public void enterPlaylistName() {
+        WebElement playlistInputField = driver.findElement(By.cssSelector("input[name='name']"));
+        playlistInputField.sendKeys(Keys.chord(Keys.COMMAND, "a", Keys.BACK_SPACE));
+        playlistInputField.sendKeys("Party Hits");
+        playlistInputField.sendKeys(Keys.ENTER);
+    }
+
+    public boolean locateNewPlaylistName() {
+        WebElement playlistElement = driver.findElement(By.xpath("//a[text()='Party Hits']"));
+        return playlistElement.isDisplayed();
+    }
+}
 
 
-//        WebElement confirmDelete = driver.findElement
-//                (By.xpath(" //div[@class='success show']"));
-//        Assert.assertTrue(confirmDelete.isDisplayed());
-//    }
-
-
-
-//    public void login( String email, String password) throws InterruptedException {
-//
-//        WebElement emailAddressField = driver.findElement(By.xpath("//input[@type='email']"));
-//        emailAddressField.sendKeys(email);
-//        Thread.sleep(2000);
-//
-//        WebElement passwordField = driver.findElement(By.xpath("//input[@type='password']"));
-//        passwordField.sendKeys(password);
-//        Thread.sleep(2000);
-//
-//        WebElement loginButton = driver.findElement(By.xpath("//button[@type='submit']"));
-//        loginButton.click();
-//
 
