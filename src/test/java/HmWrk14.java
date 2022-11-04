b31ad1ebe0576e10bfc2c908b76335e0e49a882f
jlmcswain <108906957+jlmcswain@users.noreply.github.com>
2022-11-04 08:10:56 -0700

    Jamie - Corrected Hmwrk 14

--- src/test/java/HmWrk14.java
+++ src/test/java/HmWrk14.java
@@ -9,19 +9,20 @@ import java.time.Duration;
 
 public class HmWrk14 {
     @Test
-    public static void registrationNavigation() {
+    public static void registrationNavigation() throws InterruptedException {
 
         WebDriver driver = new ChromeDriver();
         try {
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
             String url = "https://bbb.testpro.io/";
+            String regUrl = "https://bbb.testpro.io/registration.php";
             driver.get(url);
 
             WebElement regLink = driver.findElement(By.id("hel"));
             regLink.click();
 
-            WebElement regbutton = driver.findElement(By.cssSelector("[type='submit']"));
-            Assert.assertTrue(regbutton.isDisplayed());
+            Assert.assertEquals(driver.getCurrentUrl(), regUrl);
+            System.out.println("Redirected to " + driver.getCurrentUrl());
         } finally {
 
             driver.quit();
