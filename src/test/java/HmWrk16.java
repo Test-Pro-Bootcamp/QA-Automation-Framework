6faac579210d75f8a9a88ac4897b0594e510c4ac
jlmcswain <108906957+jlmcswain@users.noreply.github.com>
2022-11-04 00:45:11 -0700

    Hmwrk16
    create new playlist

---
+++ src/test/java/HmWrk16.java
@@ -0,0 +1,47 @@
+import net.bytebuddy.asm.Advice;
+import org.checkerframework.checker.units.qual.K;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class HmWrk16 {
+
+    @Test
+    public static void createNewPlaylist() {
+
+        WebDriver driver = new ChromeDriver();
+        try {
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+            String url = "https://bbb.testpro.io/";
+            driver.get(url);
+
+            By usernameLocator = By.xpath("//*[@type='email']");
+            By passwordLocator = By.xpath("//*[@type='password']");
+            By loginButton = By.xpath("//*[@type='submit']");
+            By avatarIconLocator = By.xpath("//*[@alt = 'Avatar of Student']");
+            By NewPlaylistBtn = By.xpath("//i[@title='Create a new playlist']");
+            By createNewPlaylist = By.xpath("//*[contains(text(),'New Playlist')]");
+            By newPlaylistName = By.xpath("//*[@name='name']");
+            By playlistName = By.xpath("//*[contains(text(),'MySongs')]");
+
+            driver.findElement(usernameLocator).sendKeys("demo@class.com");
+            driver.findElement(passwordLocator).sendKeys("te$t$tudent");
+            driver.findElement(loginButton).click();
+            driver.findElement(NewPlaylistBtn).click();
+            driver.findElement(createNewPlaylist).click();
+            driver.findElement(newPlaylistName).sendKeys("MySongs");
+            driver.findElement(newPlaylistName).sendKeys(Keys.RETURN);
+            Assert.assertTrue(driver.findElement(playlistName).isDisplayed());
+        } finally {
+            driver.quit();
+        }
+    }
+}
+
