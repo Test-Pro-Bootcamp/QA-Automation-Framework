53d80c687a554b56d51b8a61eb8f7329bf4a64f6
SGThel1sing <iq14111991@gmail.com>
2022-11-09 17:20:23 -0500

    Нужна помощь

--- src/test/java/Homework17.java
+++ src/test/java/Homework17.java
@@ -1,65 +1,56 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.testng.Assert;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class Homework17 extends BaseTest {
+import java.time.Duration;
 
-
-
+public class Homework17 {
     @Test
-    public void addSongToPlaylist(){
-        //login
-        provideEmail("demo@class.com");
-        providePassword("");
-        clickSubmitBtn();
-        //add a song to the playlist
-        clickViewAllBtn();
-        String getSongTitle = getSongTitleTxt();;
-        clickSongFromList();
-        clickAddToPlaylist();
-        addToSuperPlaylist();
-        clickOnSuperPlaylist();
 
-        //Verify the song
-        String songFromSuperPlaylist = getSongTitleFromSuperPlaylist();
-        Assert.assertEquals(songFromSuperPlaylist, getSongTitle);
-    }
 
-    private String getSongTitleFromSuperPlaylist() {
+    public static void addSongToPlaylist() throws InterruptedException {
 
-        WebElement superPlaylistSong = driver.findElement(By.xpath("//section[@id='playlistWrapper']//tr[@class='song-item']//td[@class='title']"));
-        return superPlaylistSong.getText();
-    }
 
-    private void clickOnSuperPlaylist() {
-        WebElement superPlaylistLink = driver.findElement(By.xpath("//a[text()='super']"));
-        superPlaylistLink.click();
-    }
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        String url = "https://bbb.testpro.io/";
+        driver.get(url);
 
-    private void addToSuperPlaylist() {
-        WebElement superPlaylist = driver.findElement(By.xpath("//section[@id='recentlyPlayedWrapper']//li[contains(text(), 'super')]"));
-        superPlaylist.click();
-    }
+        WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
+        emailField.click();
+        emailField.sendKeys("iq14111991@gmail.com");
 
-    private String getSongTitleTxt() {
-        WebElement getTitleTxt = driver.findElement(By.xpath("//section[@id='recentlyPlayedWrapper']//tr[@class='song-item']//td[@class='title']"));
-        return getTitleTxt.getText();
-    }
+        WebElement passwordField = driver.findElement(By.cssSelector("[type='password']"));
+        passwordField.click();
+        passwordField.sendKeys("te$t$tudent");
 
-    private void clickAddToPlaylist() {
-        WebElement addToPlaylist = driver.findElement(By.xpath("//button[contains(@title, 'Add selected songs')]"));
-        addToPlaylist.click();
-    }
+        WebElement submitButton = driver.findElement(By.cssSelector("[type='submit']"));
+        submitButton.click();
 
-    private void clickSongFromList() {
-        WebElement songList = driver.findElement(By.xpath("//section[@id='recentlyPlayedWrapper']//tr[@class='song-item']"));
-        songList.click();
+        WebElement choseAllSongs = driver.findElement(new By.ByXPath("//*[@id=\"sidebar\"]/section[1]/ul/li[3]/a"));
+        choseAllSongs.click();
 
-    }
 
-    private void clickViewAllBtn() {
-        WebElement viewAllBtn = driver.findElement(By.xpath("//button[@data-testid='home-view-all-recently-played-btn']"));
-        viewAllBtn.click();
+        WebElement choseSong1 = driver.findElement(new By.ByXPath("/html/body/div/div/div/section[1]/section[3]/div/div/div[1]/table/tr[1]/td[2]"));
+        choseSong1.click();
+        WebElement addTo = driver.findElement(new By.ByXPath("//*[@id=\"songsWrapper\"]/header/div[3]/span/button[2]"));
+        addTo.click();
+        Thread.sleep(3000);
+        WebElement chosePLtoAdd = driver.findElement(new By.ByXPath("//*[@id=\"songsWrapper\"]/header/div[3]/div/section[1]/ul/li[6]"));
+        chosePLtoAdd.click();
+// Я не совсем понял как можно проверить добавлена ли песня в плей лист, так же у меня есть пара вопросов:
+//        1) не получится добавить несколько песен таким способом одновременно, вводить в ручную путь для песен долго и не эффективно
+//        2) я подсмотрел как с этой задачей справились другие ученики, я не могу использовать функции так же как они
+//        private void addToSuperPlaylist() {
+//            WebElement superPlaylist = driver.findElement(By.xpath("//section[@id='recentlyPlayedWrapper']//li[contains(text(),'super')]"));
+//            superPlaylist.click();
+//        КАК пример, такие в таких функциях подчеркивает driver. Не смог понять как с этим справиться
+
+
+        Thread.sleep(3000);
+
+//        driver.quit();
     }
 }
