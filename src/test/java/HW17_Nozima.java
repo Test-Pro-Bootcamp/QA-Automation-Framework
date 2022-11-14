b8bc2d7055d3561de4c63a68ebeaa1070f5a4cb7
NozimaB <nozishka86@gmail.com>
2022-11-13 22:28:00 -0700

    HW26_Jenkins

--- src/test/java/HW17_Nozima.java
+++ src/test/java/HW17_Nozima.java
@@ -6,40 +6,29 @@ import org.testng.annotations.Test;
 public class HW17_Nozima extends BaseTest {
 
     @Test
-    public static void addSongToPlaylist() throws InterruptedException{
+    public void addSongToPlaylist(){
+        logIn("nozishka86@gmail.com","te$t$tudent");
+        createPlaylist();
 
-        enterEmail("nozishka86@gmail.com");
-        enterPassword("te$t$tudent");
-        clickSubmitBtn();
-        Thread.sleep(2000);
-
-        clickNewPlaylistBtn();
-        clickDropDown();
-        enterPlayListName();
 
         WebElement allSongs = driver.findElement(By.xpath("//a[text()='All Songs']"));
         allSongs.click();
         allSongs.click();
-        Thread.sleep(5000);
 
         WebElement song = driver.findElement(By.xpath("//tr[@class='song-item']//td[contains(text(),'a Beat')]"));
         song.click();
 
         WebElement addToBtn = driver.findElement(By.xpath("//button[@title='Add selected songs to…']"));
         addToBtn.click();
-        Thread.sleep(3000);
 
         WebElement myPlaylist = driver.findElement(By.xpath("//li[@class='playlist']"));
         myPlaylist.click();
-        Thread.sleep(3000);
 
         WebElement nozimaPlaylist = driver.findElement(By.xpath("//a[contains(text(),'Nozima')]"));
         nozimaPlaylist.click();
-        Thread.sleep(3000);
 
         WebElement addedSong = driver.findElement(By.xpath("//tr[@class='song-item']//td[contains(text(),'a Beat')]"));
         Assert.assertTrue(addedSong.isDisplayed());
-        Thread.sleep(3000);
     }
 
 }
