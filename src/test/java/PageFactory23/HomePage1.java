2f81660a86a7d224e1e2b938b0b0b7d03f118527
Yana <Dankoyanka@gmail.com>
2022-11-16 15:40:56 -0600

    PageFactory HW23 YD

---
+++ src/test/java/PageFactory23/HomePage1.java
@@ -0,0 +1,12 @@
+package PageFactory23;
+
+import org.openqa.selenium.WebDriver;
+
+public class HomePage1 extends BasePage1{
+//Locators
+
+    public HomePage1(WebDriver sentDriver) {
+        super(sentDriver);
+    }
+
+}
