e6e38a90d444910b190e77bcfd6a0a82ee2e00a0
Salma Ayad <salma.ayad.87@gmail.com>
2022-11-18 11:08:08 -0500

    update-Homework27
    cucumber

---
+++ src/test/java/Salma_POM/Pages/HomePage.java
@@ -0,0 +1,47 @@
+package Salma_POM.Pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class HomePage extends BasePage{
+
+    //Declaring Locators
+    //By avatarLocator = By.cssSelector("img.avatar");
+    //By soundBarLocator = By.xpath("//img[@alt='Sound bars']");
+    //Locators with Page Factory
+    @FindBy(css="img.avatar")
+    WebElement avatarImage;
+    @FindBy(xpath = "//img[@alt='Sound bars']")
+    WebElement soundBar;
+    //Constructor method
+    public HomePage(WebDriver givenDriver) {
+        super(givenDriver);
+        PageFactory.initElements(driver,this);
+    }
+
+    //Getting user avatar
+    public WebElement getUserAvatar(){
+        // return driver.findElement(avatarLocator);
+        return avatarImage;
+    }
+    //Method to verify user avatar is displayed
+    public boolean isUserAvatarDisplayed(){
+        //return driver.findElement(avatarLocator).isDisplayed();
+        //We don't need driver.findElement when we use page factory
+        return avatarImage.isDisplayed();
+    }
+
+    public boolean isSongPlaying(){
+//        WebElement songBarVisualizer = wait.until(ExpectedConditions.visibilityOfElementLocated(soundBarLocator));
+//         return songBarVisualizer.isDisplayed();
+        return wait.until(ExpectedConditions.visibilityOf(soundBar)).isDisplayed();
+
+    }
+}
