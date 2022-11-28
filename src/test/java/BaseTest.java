257f64c9e22acdfcb5b040d22b38465e37673305
SGThel1sing <iq14111991@gmail.com>
2022-11-28 09:14:30 -0500

    Chulkov# HW#24 Error

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,24 +1,27 @@
+import POM.FactoryPages.BasePage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.*;
 
 import java.time.Duration;
 
 public class BaseTest {
+
     WebDriver driver;
     Actions actions;
 
-    String url;
+    String baseURL;
 
     @BeforeSuite
     public static void chromeConfigs() {
         if (System.getProperty("os.name").toLowerCase().contains("win")) {
             System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        }else{
+            System.setProperty("webdriver.chrome.driver", "chromedriver");
         }
     }
 
@@ -28,12 +31,17 @@ public class BaseTest {
     }
 
     @BeforeMethod
-    public void launchBrowser() {
-        driver = new ChromeDriver();
+    @Parameters({"baseURL"})
+    public void launchBrowser(String baseURL) {
+//        driver = new ChromeDriver();
+//      driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        url = "https://bbb.testpro.io/";
-        driver.get(url);
+        if(baseURL == null)
+            baseURL = "https://bbb.testpro.io";
+        System.setProperty("webdriver.gecko.driver", "geckodriver");
+        driver = new FirefoxDriver();
         actions = new Actions(driver);
+        driver.get(baseURL);
     }
 
     public void provideEmail(String email) {
