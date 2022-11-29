b9b0dab4f6756880c5534e08fdc5dd31f84cf312
SGThel1sing <iq14111991@gmail.com>
2022-11-29 15:10:01 -0500

    ChulkovE_HW#24

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,10 +1,15 @@
 import POM.FactoryPages.BasePage;
+import java.net.MalformedURLException;
+import java.net.URI;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.*;
 
 import java.time.Duration;
@@ -32,16 +37,37 @@ public class BaseTest {
 
     @BeforeMethod
     @Parameters({"baseURL"})
-    public void launchBrowser(String baseURL) {
-//        driver = new ChromeDriver();
-//      driver = new FirefoxDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        if(baseURL == null)
+    public void launchBrowser(String baseURL) throws MalformedURLException {
+    if(baseURL == null)
             baseURL = "https://bbb.testpro.io";
-        System.setProperty("webdriver.gecko.driver", "geckodriver");
-        driver = new FirefoxDriver();
+        driver = pickBrowser(System.getProperty("browser"));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         actions = new Actions(driver);
         driver.get(baseURL);
+    }
+
+
+    private WebDriver pickBrowser(String browser) throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        String gridURL = "http://172.31.128.1:4444";
+        switch (browser){
+            case "firefox":
+                System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
+                return driver = new FirefoxDriver();
+            case "safari":
+                return driver = new SafariDriver();
+            case "grid-safari":
+                caps.setCapability("browserName", "safari");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(),caps);
+            case "grid-firefox":
+                caps.setCapability("browserName", "firefox");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(),caps);
+            case "grid-chrome":
+                caps.setCapability("browserName", "chrome");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(),caps);
+            default:
+                return driver = new ChromeDriver();
+        }
     }
 
     public void provideEmail(String email) {
