a0fbe8b107cf8c333ae612c6cfe1e0047a08e60f
YuliyaKis85 <114609759+YuliyaKis85@users.noreply.github.com>
2022-12-01 21:21:54 -0500

    updated

---
+++ src/test/java/POM/pages/HomePage.java
@@ -0,0 +1,12 @@
+package POM.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class HomePage extends POM.pages.BasePage {
+    //Locators
+
+    public HomePage(WebDriver sentDriver) {
+        super(sentDriver);
+    }
+
+}
