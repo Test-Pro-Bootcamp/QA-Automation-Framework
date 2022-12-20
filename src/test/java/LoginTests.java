631c24237890764af9377719646e2430b4270971
Denis Platonov <denis-platonov@users.noreply.github.com>
2022-12-19 19:10:20 -0800

    Update user3

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -19,7 +19,7 @@ public class LoginTests extends BaseTest {
         LoginPage loginPage = new LoginPage(driver);
         HomePage homePage = new HomePage(driver);
 
-        String username = "demo111@class.com";
+        String username = "demo@class.com";
         loginPage.provideEmail(username);
         loginPage.providePassword("te$t$tudent");
         loginPage.clickSubmitBtn();
